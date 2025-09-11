62d92dcbfc993072963b59e7f6574996b855b2ad
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2025-09-11 14:38:42 +0000

    Implementation du template

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,14 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+// Gestion du basculement de la barre latérale en mode desktop
+document.getElementById('sidebar-toggle-desktop').addEventListener('click', function () {
+    const sidebar = document.querySelector('.sidebar');
+    const mainContent = document.querySelector('.main-content');
+    sidebar.classList.toggle('collapsed');
+    mainContent.classList.toggle('collapsed');
+});
+
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
